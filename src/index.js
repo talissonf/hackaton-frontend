4606f75b60e804800e028caa069341db8deaeb6e
Talisson Ferreira <talissonf@gmail.com>
2020-05-04 19:19:58 -0300

    chore: initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//import "./styles.css";
+console.log("hello world!");
+
+const render = () => {
+
+}
+
+const router = () => {
+}
+
+const search = () => {
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  mode: 'development',
+  watch: true,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      appMountId: 'app',
+      filename: './index.html'
+    })
+  ]
+};
+
+module.exports = config;
